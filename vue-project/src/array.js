5e0b2ff9d4cf85beb16fbf269574d97f3e9c8ebb
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-03-22 10:15:52 -0400

    v

---
+++ vue-project/src/array.js
@@ -0,0 +1,50 @@
+const monkeys = [
+  {
+    species: 'Gorilla',
+    price: 40000.99,
+    InStock: 'Currently In Stock',
+    Dangerous: 'Highly Dangerous',
+    Habitat: 'Rainforest in Central Africa',
+    img: ''
+  },
+  {
+    species: 'Orangutan',
+    price: 70100.99,
+    InStock: 'Currently NOT In Stock',
+    Dangerous: 'Highly Dangerous',
+    Habitat: 'Rainforest in South East Asia',
+    img: ''
+  },
+  {
+    species: 'Chimpanzee',
+    price: 35100.99,
+    InStock: 'Currently In Stock',
+    Dangerous: 'Dangerous',
+    Habitat: 'Rainforest in Central Africa',
+    img: ''
+  },
+  {
+    species: 'Guinea Baboon',
+    price: 28050.99,
+    InStock: 'Currently Not In Stock',
+    Dangerous: 'Mildly Dangerous',
+    Habitat: 'Forest in Guinea',
+    img: ''
+  },
+  {
+    species: 'Proboscis Monkey',
+    price: 19.99,
+    InStock: 'Currently In Stock',
+    Dangerous: 'Not Dangerous',
+    Habitat: 'Rainforest in South East Asia',
+    img: ''
+  },
+  {
+    species: 'Golden Snub-Nosed Monkey',
+    price: 20700.99,
+    InStock: 'Currently In Stock',
+    Dangerous: 'Not Dangerous',
+    Habitat: 'South West China',
+    img: ''
+  }
+]
